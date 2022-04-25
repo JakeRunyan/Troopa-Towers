ba6a2507ac3fc85017e50b0915ba0cc6daf5bcbf
QuanAloha <nathaniel.paul.2018@icloud.com>
2022-04-25 12:38:01 -0600

    More GUI Updates

--- src/game/StartButton.java
+++ src/game/StartButton.java
@@ -1,5 +1,25 @@
 package game;
 
-public class StartButton {
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class StartButton implements Animatable {
+
+    @Override
+    public void update(double elapsedTime) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void draw(Graphics g, GameView view) {      
+        // Draw the text
+        g.setColor(Color.WHITE);
+        g.drawString("Press Lakitu to start the game", 650, 700);
+
+        // Draw the Image
+        view.drawCenteredImage(g, "resources/mario.png", 700, 600, 35);
+        
+    }
     
 }
